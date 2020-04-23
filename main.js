1c928c4e060d9c72d348464ca1c4cadf1eaf9ca4
Kris <sichinava.ch@gmail.com>
2020-04-23 13:21:27 +0300

    Added Gameboard Display Controller

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-let gameboard = {
-    array   :  [X,O,X,X,O,X,O,O,X]
-};
+const cells = document.querySelectorAll(".cell");
+const cross= `<img src="Cross.svg"`;
+const circle= `<img src="Circle.svg`;
+console.log(cells);
+let gameboard = (function(){
+    let array = ["X","O","X","X","O","X","O","O","X"];
+    return {
+        displayBoard : function(){
+            cells.forEach((cell,index)=>{
+                cell.innerHTML=`<img src="${array[index]==="X"? "Cross" : "Circle"}.svg">`;
+            })
+
+        },
+    }
+})();
+gameboard.displayBoard();
 let player1 = {
 
 }
