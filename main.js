1bd618c0ff695e57676d527c16fe895ad5924987
Kris <sichinava.ch@gmail.com>
2020-04-24 11:44:20 +0300

    Win Detection and Scores complete

--- main.js
+++ main.js
@@ -16,6 +16,58 @@ let gameBoard = (function(){
             })
 
         },
+        checkBoard : function(){
+            if ((array[0]==="X") && (array[0]===array[1] && array[1]===array[2])){
+                for(i=0;i<=2;i++){
+                 cells[i].style.backgroundColor="red";}
+                 return 1;} 
+            else if((array[3]==="X") && (array[3]===array[4] && array[4]===array[5])){
+                for(i=3;i<=5;i++){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if((array[6]==="X") && (array[6]===array[7] && array[7]===array[8])){
+                for(i=6;i<=8;i++){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if((array[0]==="X") && (array[0]===array[3] && array[3]===array[6])){
+                for(i=0;i<=6;i+=3){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if((array[1]==="X") && (array[1]===array[4] && array[4]===array[7])){
+                for(i=1;i<=7;i+=3){
+                 cells[i].style.backgroundColor="red";} return 1;} 
+            else if((array[2]==="X") && (array[2]===array[5] && array[5]===array[8])){
+                for(i=2;i<=8;i+=3){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if((array[0]==="X") && (array[0]===array[4] && array[4]===array[8])){
+                for(i=0;i<=8;i+=4){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if((array[2]==="X") && (array[2]===array[4] && array[4]===array[6])) {
+                for(i=2;i<=6;i+=2){
+                 cells[i].style.backgroundColor="red";} return 1;}
+            else if ((array[0]==="O") && (array[0]===array[1] && array[1]===array[2])){
+                for(i=0;i<=2;i++){
+                 cells[i].style.backgroundColor="red";} return 2; }
+            else if((array[3]==="O") && (array[3]===array[4] && array[4]===array[5])){
+                for(i=3;i<=5;i++){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[6]==="O") && (array[6]===array[7] && array[7]===array[8])){
+                for(i=6;i<=8;i++){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[0]==="O") && (array[0]===array[3] && array[3]===array[6])){
+                for(i=0;i<=6;i+=3){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[1]==="O") && (array[1]===array[4] && array[4]===array[7])){
+                for(i=1;i<=7;i+=3){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[2]==="O") && (array[2]===array[5] && array[5]===array[8])){
+                for(i=2;i<=8;i+=3){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[0]==="O") && (array[0]===array[4] && array[4]===array[8])){
+                for(i=0;i<=8;i+=4){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else if((array[2]==="O") && (array[2]===array[4] && array[4]===array[6])) {
+                for(i=2;i<=6;i+=2){
+                 cells[i].style.backgroundColor="red";} return 2;}
+            else return "Draw";
+        },
         placeMarker : function(marker,index){
             array[index]=marker;
         },
@@ -30,25 +82,42 @@ let gameBoard = (function(){
     }
 })();
 
-const Player = (name) =>{
+const Player = (name,score) =>{
     const getName = () => console.log(name);
-    return {getName};
+    const getScore = () => score;
+    const incScore = () => ++score;
+    return {getName,getScore,incScore};
 };
 
-const Player1 = Player("Player1");
-const Player2 = Player("Player2");
-
-Player1.getName();
-Player2.getName();
-
+const Player1 = Player("Player1",0);
+const Player2 = Player("Player2",0);
 
 
 const gameFlow = (function (){
+        const firstScore= document.querySelector("#firstScore");
+        const secondScore=document.querySelector("#secondScore");
+        let newGame = function(){
+            gameBoard.clearArray();
+            gameBoard.displayBoard();
+        }
+        let declareWinner = function(player){
+            if (player === 1) {
+                Player1.incScore();
+                firstScore.textContent=Player1.getScore();
+            }
+            else if (player === 2){
+                Player2.incScore();
+                secondScore.textContent=Player2.getScore();
+            }
+            else {
+                console.log("It's a draw!");
+            }
+        }
     return{
         startGame : function(){
             const restartButton = document.querySelector("#resetButton");
             restartButton.addEventListener('click',()=>{
-                this.newGame();
+                newGame();
             })
             let toggle = true;
             gameBoard.displayBoard();
@@ -57,19 +126,29 @@ const gameFlow = (function (){
                      if(toggle===true && gameBoard.showArray()[index]===""){
                          gameBoard.placeMarker("X",index);
                          toggle=!toggle;   
-                     }
+                    }
                      else if(toggle===false && gameBoard.showArray()[index]===""){
                          gameBoard.placeMarker("O",index);
                          toggle=!toggle;  
                      }
                      gameBoard.displayBoard();
+                     if (gameBoard.checkBoard() === 1 ){
+                        console.log("Hi");
+                       declareWinner(1);
+                   }
+                   else if (gameBoard.checkBoard() === 2 ){
+                       console.log("Hi");
+                      declareWinner(2);
+                   }
+                   else {
+                       declareWinner("Draw");
+                   }
+                     
+                     
                  })
             })
         },
-        newGame :   function(){
-            gameBoard.clearArray();
-            gameBoard.displayBoard();
-        }
+        
     }
         })();
 
